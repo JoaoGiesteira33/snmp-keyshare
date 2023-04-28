0488aa1288a10b929513d05ba9a52003a111740c
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2023-04-28 11:46:14 +0100

    Key generation finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,25 @@ package main.java;
 
 class Main{
     public static void main(String[] args){
-        Matrix a = new Matrix(4, new byte[]{1,2,3,4,5,6,7,8});
+        byte[] key = new byte[]{1,2,3,4,5,6,7,8};
+
+        Matrix a = new Matrix(4, key, 0);
+        Matrix b = new Matrix(4, key, 1);
+        Matrix s = new Matrix(4, 650487);
+        Matrix xored = new Matrix(4, a.getMatrix(), b.getMatrix(), s.getMatrix());
+        
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(xored);
+
+        xored.update_matrix();
+        System.out.println(xored);
+
+        byte[] generated_key = xored.generate_key(4);
+        for(int i = 0; i < generated_key.length; i++){
+            String to_print = String.format("0x%02X ", generated_key[i]);
+            System.out.print(to_print);
+        }
     }
 }
--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,10 +1,14 @@
 package main.java;
 
+import java.util.Random;
+
 public class Matrix {
     private byte[][] matrix;
     private byte[] key;
+    private int update_count = 0;
     
-    public Matrix(int size, byte[] key){
+    //Type 0 or Type 1 matrix
+    public Matrix(int size, byte[] key, int type){
         this.matrix = new byte[size][size];
         this.key = key;
 
@@ -20,15 +24,142 @@ public class Matrix {
             m2[i] = this.key[i + size];
         }
 
+        if(type == 0){
+            type_0_matrix_creation(m1, size);
+        }else if(type == 1){
+            type_1_matrix_creation(m2, size);
+        }
+    
+    }
+
+    //Random matrix with a given seed
+    public Matrix(int size, long seed){
+        this.matrix = new byte[size][size];
+
+        Random rand = new Random(seed);
+        final int min = 0;
+        final int max = 255;
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                this.matrix[i][j] = (byte)(rand.nextInt(max + 1 - min) + min);
+            }
+        }
+
+    }
+
+    //XOR of 3 matrices
+    public Matrix(int size, byte[][] matrix_a, byte[][] matrix_b, byte[][] matrix_s){
+        this.matrix = new byte[size][size];
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                this.matrix[i][j] = (byte)(matrix_a[i][j] ^ matrix_b[i][j] ^ matrix_s[i][j]);
+            }
+        }
+    }
+
+    //Rows based on first half of key
+    private void type_0_matrix_creation(byte[] m1, int size){
         //First row is first half of the key
         for(int i = 0 ; i < size; i++){
-            matrix[0][i] = m1[i];
+            this.matrix[0][i] = m1[i];
         }
 
         //Following rows are rotations of m1
         for(int i = 1 ; i < size; i++){
-            matrix[i] = rotate_right(m1, i);
+            this.matrix[i] = rotate_right(m1, i);
         }
+    }
+
+    //Columns based on second half of key
+    private void type_1_matrix_creation(byte[] m2, int size){
+        //First column is second half of the key
+        for(int i = 0 ; i < size; i++){
+            this.matrix[i][0] = m2[i];
+        }
+
+        //Following columns are rotations of m2
+        for(int i = 1 ; i < size; i++){
+            byte[] new_column = rotate_right(m2, i);
+
+            for(int j = 0; j < size; j++){
+                this.matrix[j][i] = new_column[j];
+            }
+        }
+    }
+
+    public byte[][] getMatrix(){
+        byte[][] new_matrix = new byte[this.matrix.length][this.matrix.length];
+
+        for(int i = 0; i < this.matrix.length; i++){
+            for(int j = 0; j < this.matrix.length; j++){
+                new_matrix[i][j] = this.matrix[i][j];
+            }
+        }
+
+        return new_matrix;
+    }
+
+    public byte[] getColumn(int index){
+        byte[] column = new byte[this.matrix.length];
+
+        for(int i = 0; i < this.matrix.length; i++){
+            column[i] = this.matrix[i][index];
+        }
+
+        return column;
+    }
+
+    public void update_matrix(){
+        int size = this.matrix.length;
+
+        //Rotate rows randomly
+        for(int i = 0; i < size; i++){
+            byte first_row_value = this.matrix[i][0];
+            Random rand = new Random((long)first_row_value);
+            int rotate_value = rand.nextInt(size);
+
+            this.matrix[i] = rotate_right(this.matrix[i], rotate_value);
+        }
+
+        //Rotate columns randomly
+        for(int j = 0 ; j < size; j++){
+            byte first_column_value = this.matrix[0][j];
+            Random rand = new Random((long)first_column_value);
+            int rotate_value = rand.nextInt(size);
+
+            byte[] new_column = rotate_right(this.getColumn(j), rotate_value);
+
+            for(int i = 0; i < size; i++){
+                this.matrix[i][j] = new_column[i];
+            }
+        }
+
+        this.update_count++;
+    }
+
+    public byte[] generate_key(int size){
+        byte[] key = new byte[size];
+
+        long seed = this.update_count + this.matrix[0][0];
+        Random rand = new Random(seed);
+        int row = rand.nextInt(size);
+
+        seed = this.matrix[row][0];
+        rand = new Random(seed);
+        int column = rand.nextInt(size);
+
+        System.out.println("Row: " + row + " Column: " + column);
+
+        byte[] row_bytes = this.matrix[row];
+        byte[] column_bytes = this.getColumn(column);
+
+        for(int i = 0; i < size; i++){
+            key[i] = (byte)(row_bytes[i] ^ column_bytes[i]);
+        }
+
+        return key;
     }
 
     private byte[] rotate_right(byte[] line, int value){
@@ -47,7 +178,7 @@ public class Matrix {
 
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix.length; j++){
-                sb.append(matrix[i][j]);
+                sb.append(String.format("0x%02X ", matrix[i][j]));
                 sb.append(" ");
             }
             sb.append("\n");
