b760df7855f908f187b3d68933ae1d934ead64be
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2023-05-06 17:56:40 +0100

    Construcao Agente inicial

--- src/main/java/Agente.java
+++ src/main/java/Agente.java
@@ -1,5 +1,69 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Agente {
-    
+    private static final String DEFAULT_CONFIG_FILE = "default.conf";
+    private static final long SEED = 33;
+
+    private int K; //Tamanho matriz
+    private byte[] M; //Chave geração matriz
+    private int T; //Intervalo entre atualização da matriz (ms)
+    private int V; //Tempo validade Chaves, etc. (s)
+    private int X; //Máximo entradas tabela de informações
+
+    private Matrix matrix;
+
+    public Agente(String config_file){
+        read_file(config_file);
+        gerar_matriz();
+    }
+
+    private void read_file(String file_name){
+        String config_files_folder = "config_files";
+        String file_path = config_files_folder + "/" + file_name;
+
+        try{
+            File configFile = new File(file_path);
+            Scanner myReader = new Scanner(configFile);
+
+            String K = myReader.nextLine();
+            String M = myReader.nextLine();
+            String T = myReader.nextLine();
+            String V = myReader.nextLine();
+            String X = myReader.nextLine();   
+
+            if(myReader.hasNextLine()){
+                System.out.println("Ficheiro de configuração inválido!");
+                System.out.println("Utilizando valores por default.");
+                read_file(DEFAULT_CONFIG_FILE);
+            }
+
+            myReader.close();
+            //Parse de valores lidos
+            this.K = Integer.parseInt(K);
+            this.M = M.getBytes();
+            this.T = Integer.parseInt(T);
+            this.V = Integer.parseInt(V);
+            this.X = Integer.parseInt(X);
+        }catch(FileNotFoundException e){
+            System.out.println("Ficheiro de configuração não encontrado!");
+            System.out.println("Utilizando valores por default.");
+            read_file(DEFAULT_CONFIG_FILE);
+        }
+    }
+
+    private void gerar_matriz(){
+        Matrix a = new Matrix(this.K, this.M, 0);
+        Matrix b = new Matrix(this.K, this.M, 1);
+        Matrix s = new Matrix(this.K, SEED);
+
+        this.matrix = new Matrix(this.K, a.getMatrix(), b.getMatrix(), s.getMatrix());
+    }
+
+    public void print_matrix(){
+        System.out.println(this.matrix.toString());
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,10 @@
 package main.java;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 class Main{
     public static void main(String[] args){
-        byte[] key = new byte[]{1,2,3,4,5,6,7,8};
-
-        Matrix a = new Matrix(4, key, 0);
-        Matrix b = new Matrix(4, key, 1);
-        Matrix s = new Matrix(4, 650487);
-        Matrix xored = new Matrix(4, a.getMatrix(), b.getMatrix(), s.getMatrix());
-        
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(s);
-        System.out.println(xored);
-
-        xored.update_matrix();
-        System.out.println(xored);
-
-        byte[] generated_key = xored.generate_key(4);
-        for(int i = 0; i < generated_key.length; i++){
-            String to_print = String.format("0x%02X ", generated_key[i]);
-            System.out.print(to_print);
-        }
+        Agente a = new Agente("default.conf");
+        a.print_matrix();
     }
 }
